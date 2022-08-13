708d4249789f810a48d3b56fd30de93aae74f759
Dominik <dominik.graf2001@gmail.com>
2022-08-13 09:15:07 +0200

    added images and started to work on slide show

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,37 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'dominik';
+export class AppComponent implements OnInit  { // implemntieren OnIniti von der Libary Angular
+
+images = [`business.jpg`,`laptop.jpg`,`scool.jpg`];
+currentImage=0;
+showImage = true;
+
+ngOnInit(){
+this.updateImage();
+}
+//NgOnInit wird immer am anfang ausgeführt 
 
 
-
-
+updateImage() {
   
+setInterval(()=>{
+this.currentImage++;
+this.currentImage =this.currentImage % this.images.length; // % modulu
+this.showImage =false;
+
+setTimeout(()=>{
+  this.showImage =true;
+},10)
+
+},8000)
+
+}
+
+
+
 }
